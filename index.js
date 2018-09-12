42e62293a73de22fb45cc68bb4eac13d4d6bc5ea
Jeff Carter <jeff@pepperfly.com>
2018-09-12 20:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ function preserveDecimal(n) {
     return `NaN`;
   }
   var integr = Math.floor(n);
-  return (parseFloat(n) - integr).toFixed(2);
+  return (parseFloat(n) - integr).toFixed(3);
 }
